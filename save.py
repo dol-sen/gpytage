794cfaeeb8435eaee204f1a3a2b57a1a88f6cf69
ken69267 <>
2007-12-31 23:38:15 +0000

    Changed save.py to a class structure. Added IOError Error Dialog for saving files.

--- save.py
+++ save.py
@@ -26,64 +26,74 @@ import sys
 import pdb
 
 from helper import portage_path, reload
-from window import title, window
+from window import title, window, createMessageDialog
+import gtk
 
-def save(): #the important one...
-	import datastore
-	lists = datastore.lists
-	for name,store in lists.iteritems():
-		try:
-			if name == store[0][3]:#I'm a main file
-				file = name
-				data = []
-				parent = None
-				for row in store:
-					datarow = assemblerow(row)
-					data.append(datarow)
-				savefile(parent, file, data)
-			else: #we have a subfile
-				parent = store[0][3] #main dir
-				file = name #sub file
-				data = []
-				for row in store:
-					datarow = assemblerow(row)
-					data.append(datarow)
-				savefile(parent, file, data)
-		except IndexError:
-			print 'Failed to save the file %s for write access' % name
-	reload()
-#insight: datastore can be thought of a giant list, where row[0] references the first item in a multi list list. eg: foo = [['blah'],['blah1']]
+class SaveFile():
+	def __init__(self):
+		self.errors = []
+		
+	def save(self): #the important one...
+		import datastore
+		lists = datastore.lists
+		for name,store in lists.iteritems():
+			try:
+				if name == store[0][3]:#I'm a main file
+					file = name
+					data = []
+					parent = None
+					for row in store:
+						datarow = self.assemblerow(row)
+						data.append(datarow)
+					self.savefile(parent, file, data)
+				else: #we have a subfile
+					parent = store[0][3] #main dir
+					file = name #sub file
+					data = []
+					for row in store:
+						datarow = self.assemblerow(row)
+						data.append(datarow)
+					self.savefile(parent, file, data)
+			except IndexError:
+				print 'Failed to save the file %s for write access' % name
+		reload()
+		if self.errors != []:
+			#spawn dialog
+			err = ',\n'.join(self.errors)
+			message = "The following files failed to save:\n\n%s. \n\nPossible causes may include insufficient privileges to write to these files." %err
+			createMessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, "Erroring Saving...", message)
+	#insight: datastore can be thought of a giant list, where row[0] references the first item in a multi list list. eg: foo = [['blah'],['blah1']]
 
-def assemblerow(child):
-	try:
-		len(child[0])
-		text1 = child[0]
-	except:
-		text1 = ""
-	try:
-		len(child[1])
-		text2 = child[1]
-	except:
-		text2 = ""
-	datarow = text1 + " " + text2 + '\n'
-	return datarow
+	def assemblerow(self, child):
+		try:
+			len(child[0])
+			text1 = child[0]
+		except:
+			text1 = ""
+		try:
+			len(child[1])
+			text2 = child[1]
+		except:
+			text2 = ""
+		datarow = text1 + " " + text2 + '\n'
+		return datarow
 
-def savefile(parent, file, data):
-	if parent is None: #main file
-		try:
-			f=open(portage_path + file, 'w')
-		except IOError:
-			print 'Failed to open '+ portage_path + file + ' for write access'
-			return
-		for row in data:
-			f.write(row)
-		f.close
-	else: #subfile
-		try:
-			f=open(portage_path + parent + '/' + file, 'w')
-		except IOError:
-			print 'failed to open '+ portage_path + parent + '/' + file + ' for write access'
-			return
-		for row in data:
-			f.write(row)
-		f.close
+	def savefile(self, parent, file, data):
+		if parent is None: #main file
+			try:
+				f=open(portage_path + file, 'w')
+				for row in data:
+					f.write(row)
+				f.close
+			except IOError:
+				self.errors.append("%s%s" % (portage_path, file))
+				return
+		else: #subfile
+			try:
+				f=open(portage_path + parent + '/' + file, 'w')
+				for row in data:
+					f.write(row)
+				f.close
+			except IOError:
+				self.errors.append("%s%s/%s" %(portage_path, parent, file))
+				return
