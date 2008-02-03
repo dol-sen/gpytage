8ee88da9de21ef6ac610f81058410751a50b174b
ken69267 <>
2008-02-03 15:49:23 +0000

    Patching setup.py wrt bug #10817

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from version import version as p_version
 
 setup(name="gpytage",
-	version="0.2.0_beta",
-	description="GTK Utility to help manage Portage's package files",
+	version=p_version,
+	description="GTK Utility to help manage Portage's user config files",
 	license="GPL-2",
 	author="Kenneth Prugh",
 	author_email="ken69267@gmail.com",
 	url="https://gna.org/projects/gpytage/",
-	download_url="http://download.gna.org/gpytage/gpytage-0.2.0_beta.tar.gz",
+	download_url="http://download.gna.org/gpytage/gpytage-" + p_version + ".tar.gz",
 	package_dir={'gpytage':''},
 	packages=['gpytage'],
 	scripts=["gpytage"],
