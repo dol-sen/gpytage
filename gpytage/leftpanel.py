905fde2c37b195ab964f1aa8b5ac33edc429f5b0
Kenneth Prugh <ken69267@gmail.com>
2009-05-12 20:02:39 -0400

    Updated leftpanel for displaying recursive folders
    Lots of cruft deleted. Some temporarily commented out or broken (such
    as __clicked()) until the rightpanel is updated.

--- gpytage/leftpanel.py
+++ gpytage/leftpanel.py
@@ -24,69 +24,51 @@
 import pygtk; pygtk.require("2.0")
 import gtk
 import datastore
-from datastore import E_NAME, E_DATA, E_EDITABLE, E_PARENT, E_MODIFIED
-import rightpanel
-from panelfunctions import switchListView
+from datastore import F_NAME, F_REF
+#===============================================================================
+# import rightpanel
+# from panelfunctions import switchListView
+#===============================================================================
 
-leftview = gtk.TreeView(datastore.datastore) #create the container
+leftview = gtk.TreeView(datastore.folderModel) #create the container
 
-leftview.set_search_column(E_NAME)
-#columns
+leftview.set_search_column(F_NAME)
+
+# TreeViewColumns
 namecol = gtk.TreeViewColumn('Package File')
-testcol = gtk.TreeViewColumn('Flags')
-boolcol = gtk.TreeViewColumn() #editable col
-filecol = gtk.TreeViewColumn()
-#add to tree
+
+# add TreeViewColumn to TreeView
 leftview.append_column(namecol)
-leftview.append_column(testcol)
-leftview.append_column(boolcol)
-leftview.append_column(filecol)
 
-#render cell
 cell = gtk.CellRendererText()
-cell1 = gtk.CellRendererText()
 
-#add cols to cell
+# add CellRenderer to TreeViewColumn
 namecol.pack_start(cell, True)
-namecol.set_attributes(cell, text=E_NAME)
-namecol.add_attribute(cell, "editable", E_EDITABLE)#set row editable
+namecol.add_attribute(cell, 'text', F_NAME)
 namecol.set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
 
-boolcol.set_visible(False)
-filecol.set_visible(False)
-
-testcol.pack_start(cell1, True)
-testcol.set_attributes(cell1, text=1)
-testcol.add_attribute(cell1, "editable", E_EDITABLE)#set row editable
-testcol.set_expand(True)
-testcol.set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
-testcol.set_visible(False)
-
-###########Scroll Window#########################
+#===============================================================================
+# Scroll Window
+#===============================================================================
 scroll = gtk.ScrolledWindow()
 scroll.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 scroll.add_with_viewport(leftview)
 
-############Drag and Drop####################
-#note: is DND even needed for left panel?
-leftview.enable_model_drag_dest([('text/plain', 0, 0)], gtk.gdk.ACTION_DEFAULT)
-import panelfunctions
-leftview.connect("drag_data_received", panelfunctions.get_dragdestdata)
-leftview.connect("drag-motion", switchListView)
+# Allows us to check if the user simply clicked on the same file or on another one
+__lastSelected = None
 
-###########some variables####################
-last_parent = None
-
-def _clicked(treeview, *args):
-	""" Handle treeview clicks """
-	global last_parent
+def __clicked(treeview, *args):
+	""" Handle TreeView clicks """
+	global __lastSelected
 	model, iter = treeview.get_selection().get_selected()
-	if iter: parent = model.get_value(iter,E_PARENT).strip('*')
-	else: parent = last_parent.strip('*')
+	if iter: # None if no row is selected 
+		parent = model.get_value(iter, F_REF).getName()
+	else: 
+		parent = lastSelected
 	# has the selection really changed?
-	if parent.strip('*') != last_parent:
+	if parent != last_parent:
 		print("LEFTPANEL: parent change detected")
-		list = model.get_value(iter, E_NAME).strip('*')
+		list = model.get_value(iter, F_NAME).strip('*')
 		print list
 		print parent
 		if parent.strip('*') == 'package.' + list:
@@ -97,7 +79,7 @@ def _clicked(treeview, *args):
 		list = model.get_value(iter, E_NAME).strip('*')
 		rightpanel.setListModel(list)
 	# save current selection as last selected
-	last_parent = parent.strip('*')
+	lastSelected = parent
 
-#Signals
-leftview.connect("cursor-changed", _clicked)
+# Signals
+leftview.connect("cursor-changed", __clicked)
