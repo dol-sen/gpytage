8993a3ade977d3851cc74e1383c460450009a068
Kenneth Prugh <ken69267@gmail.com>
2009-12-04 16:28:33 -0500

    Clear internel ListStore on method initData()

--- gpytage/PackageFileObj.py
+++ gpytage/PackageFileObj.py
@@ -41,7 +41,8 @@ class PackageFileObj:
         
     def initData(self):
         """ Read contents of File into ListStore """
-        rawData = self.scanFileContents(self.path)
+        self.data.clear()
+        rawData = self.__scanFileContents(self.path)
         for line in rawData:
             try:
                 c1 = line[L_NAME].rstrip()
@@ -82,7 +83,7 @@ class PackageFileObj:
         """ Returns the filepath for this PackageFileObj """
         return self.path
         
-    def scanFileContents(self, filepath):
+    def __scanFileContents(self, filepath):
         """ Return data in specified file in a list of a list [[col1, col2]]"""
         try:
             f=open(filepath, 'r')
