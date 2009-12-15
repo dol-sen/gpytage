1b71f2fd2a63e8a71028c1265a3fd2aec9e8a5f3
Kenneth Prugh <ken69267@gmail.com>
2009-12-14 20:12:03 -0500

    Rename now spawns a gtk.FileChooserDialog with overwrite_confirmation on

--- gpytage/rename.py
+++ gpytage/rename.py
@@ -34,28 +34,20 @@ def renameFile(*args):
         print >>stderr, "__renameFile:",e
 
 def __createRenameDialog(object, type, setFolder):
-    d = gtk.Dialog("Rename " + type + "...", None, gtk.DIALOG_MODAL,\
-            (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
-    d.set_default_response(gtk.RESPONSE_NO)
-    dVbox = d.get_content_area()
-    # create our filechooser widget
-    fc = gtk.FileChooserWidget(gtk.FILE_CHOOSER_ACTION_SAVE, None)
+    fc = gtk.FileChooserDialog("Rename file...", None,
+            gtk.FILE_CHOOSER_ACTION_SAVE, (gtk.STOCK_CANCEL,
+                gtk.RESPONSE_CANCEL, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+    fc.set_do_overwrite_confirmation(True)
     fc.set_filename(object.getPath()) #Set the fc to the object to be renamed
-    dVbox.pack_start(gtk.Label("Renaming " + object.getPath()))
-    dVbox.pack_start(fc)
-    #show the widget
-    d.show_all()
-    response = d.run()
+    fc.set_extra_widget(gtk.Label("Renaming " + object.getPath()))
+    response = fc.run()
     # todo return the value to renameFile and handle accordingly
     if response == gtk.RESPONSE_ACCEPT:
         if fc.get_filename() != None:
             print object.getPath() + " renamed to " + fc.get_filename()
-            d.destroy()
         else:
             print >>stderr, "Invalid rename request"
-            d.destroy()
-    else:
-        d.destroy()
+    fc.destroy()
 
 def __ensureNotModified():
     if hasModified():
