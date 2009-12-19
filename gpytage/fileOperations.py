3960b49ce05d934e4e60640f0240317649dbb401
Kenneth Prugh <ken69267@gmail.com>
2009-12-19 11:40:17 -0500

    Fix year in header

--- gpytage/fileOperations.py
+++ gpytage/fileOperations.py
@@ -3,7 +3,7 @@
 #   fileOperations.py GPytage module
 #
 ############################################################################
-#    Copyright (C) 2008-2009 by Kenneth Prugh                              #
+#    Copyright (C) 2009 by Kenneth Prugh                                   #
 #    ken69267@gmail.com                                                    #
 #                                                                          #
 #    This program is free software; you can redistribute it and#or modify  #
