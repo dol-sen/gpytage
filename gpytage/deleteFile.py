e716929671f6ea891c803e5649bb51196f30a35a
Kenneth Prugh <ken69267@gmail.com>
2009-12-19 11:35:21 -0500

    header updates

--- gpytage/deleteFile.py
+++ gpytage/deleteFile.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
-
-# GPL2 by Kenneth Prugh <ken69267@gmail.com>
+#
+#   deleteFile.py GPytage module
+#
+############################################################################
+#    Copyright (C) 2009 by Kenneth Prugh                                   #
+#    ken69267@gmail.com                                                    #
+#                                                                          #
+#    This program is free software; you can redistribute it and#or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation under version 2 of the license.          #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
 
 import pygtk; pygtk.require("2.0")
 import gtk
